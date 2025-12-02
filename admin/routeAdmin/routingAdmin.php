ecd3ad634a7cd443636e607d5b320a760719255b
Roman Jefimov <roman.jefimov@ivkhk.ee>
2025-12-02 14:36:32 +0200

    Добавленно редактирование

--- admin/routeAdmin/routingAdmin.php
+++ admin/routeAdmin/routingAdmin.php
@@ -27,6 +27,13 @@ elseif($path=='newsAdd') {
 elseif($path == 'newsAddResult') {
     $response = controllerAdminNews::newsAddResult();
 }
+//------------------edit news
+elseif($path=='newsEdit' && isset($_GET['id'])) {
+    $response=controllerAdminNews::newsEditForm($_GET['id']);
+}
+elseif($path == 'newsEditResult' && isset($_GET['id'])) {
+    $response = controllerAdminNews::newsEditResult($_GET['id']);
+}
 else {
     //Страница не существует
     $response = controllerAdmin::error404();
